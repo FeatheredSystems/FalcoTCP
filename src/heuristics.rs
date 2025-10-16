464ecae293025f30dd648c4b819add22b0396b59
Théo <theo.dev.1234567890@gmail.com>
2025-10-16 21:38:34 +0000

    Fixed issue related to: cargo check --all-features

--- src/heuristics.rs
+++ src/heuristics.rs
@@ -1,136 +1,134 @@
 use crate::enums::CompressionAlgorithm;
 
-#[cfg(all(feature = "heuristics", feature = "heubias-performance"))]
 #[inline]
-#[allow(unused_variables)]
-pub const fn get_compressor(size: usize) -> CompressionAlgorithm {
-    #[cfg(feature = "LZ4")]
-    return CompressionAlgorithm::Lz4;
-    #[cfg(not(feature = "LZ4"))]
+#[allow(unreachable_code)]
+pub const fn get_compressor(size: usize) -> CompressionAlgorithm{
+    // heubias-performance
+    #[cfg(all(feature="heubias-performance",not(feature="heubias-ratio")))]
     {
-        #[cfg(feature = "ZSTD")]
-        return CompressionAlgorithm::Zstd;
-        #[cfg(not(feature = "ZSTD"))]
+        #[cfg(feature = "LZ4")]
+        return CompressionAlgorithm::Lz4;
+        #[cfg(not(feature = "LZ4"))]
         {
+            #[cfg(feature = "ZSTD")]
+            return CompressionAlgorithm::Zstd;
+            #[cfg(not(feature = "ZSTD"))]
+            {
+                #[cfg(feature = "GZIP")]
+                return CompressionAlgorithm::Gzip;
+                #[cfg(not(feature = "GZIP"))]
+                return CompressionAlgorithm::None;
+            }
+        }
+    }
+    #[cfg(all(feature="heubias-ratio",not(feature="heubias-performance")))]
+    {
+        #[cfg(any(feature = "LZMA", feature = "GZIP", feature = "ZSTD", feature = "LZ4"))]
+        if size < 10485760  {
+            #[cfg(feature = "LZMA")]
+            {
+                return CompressionAlgorithm::Lzma;
+            }
             #[cfg(feature = "GZIP")]
-            return CompressionAlgorithm::Gzip;
-            #[cfg(not(feature = "GZIP"))]
-            return CompressionAlgorithm::None;
+            {
+                return CompressionAlgorithm::Gzip;
+            }
+            #[cfg(feature = "ZSTD")]
+            {
+                return CompressionAlgorithm::Zstd;
+            }
+            #[cfg(feature = "LZ4")]
+            {
+                return CompressionAlgorithm::Lz4;
+            }
         }
+        #[cfg(any(feature = "GZIP", feature = "ZSTD", feature = "LZ4"))]
+        if size > 10485760 && size < 209715200 {
+            #[cfg(feature = "GZIP")]
+            {
+                return CompressionAlgorithm::Gzip;
+            }
+            #[cfg(feature = "ZSTD")]
+            {
+                return CompressionAlgorithm::Zstd;
+            }
+            #[cfg(feature = "LZ4")]
+            {
+                return CompressionAlgorithm::Lz4;
+            }
+        }
+        #[cfg(any(feature = "ZSTD", feature = "GZIP"))]
+        if size >= 209715200 && size < 314572800 {
+            #[cfg(feature = "ZSTD")]
+            {
+                return CompressionAlgorithm::Zstd;
+            }
+            #[cfg(feature = "GZIP")]
+            {
+                return CompressionAlgorithm::Gzip;
+            }
+        }
+        #[cfg(feature = "LZ4")]
+        if size >= 314572800 {
+            // 300 mib
+            #[cfg(feature = "LZ4")]
+            {
+                return CompressionAlgorithm::Lz4;
+            }
+        }
+        CompressionAlgorithm::None
+
+    }
+    #[cfg(any(all(feature="heubias-ratio",feature="heubias-performance"),not(any(all(feature="heubias-ratio",feature="heubias-performance")))))]
+    {
+        #[cfg(any(feature = "LZMA", feature = "ZSTD", feature = "GZIP", feature = "LZ4"))]
+        if size < 10485760 {
+            #[cfg(feature = "LZMA")]
+            {
+                return CompressionAlgorithm::Lzma;
+            }
+            #[cfg(feature = "ZSTD")]
+            {
+                return CompressionAlgorithm::Zstd;
+            }
+            #[cfg(feature = "GZIP")]
+            {
+                return CompressionAlgorithm::Gzip;
+            }
+            #[cfg(feature = "LZ4")]
+            {
+                return CompressionAlgorithm::Lz4;
+            }
+        }
+        #[cfg(any(feature = "ZSTD", feature = "GZIP", feature = "LZMA"))]
+        if size >= 10485760 && size < 209715200 {
+            #[cfg(feature = "ZSTD")]
+            {
+                return CompressionAlgorithm::Zstd;
+            }
+            #[cfg(feature = "LZMA")]
+            {
+                return CompressionAlgorithm::Lzma;
+            }
+            #[cfg(feature = "GZIP")]
+            {
+                return CompressionAlgorithm::Gzip;
+            }
+        }
+        #[cfg(any(feature = "ZSTD", feature = "LZMA"))]
+        if size >= 209715200 {
+            #[cfg(feature = "ZSTD")]
+            {
+                return CompressionAlgorithm::Zstd;
+            }
+            #[cfg(feature = "LZMA")]
+            {
+                return CompressionAlgorithm::Lzma;
+            }
+        }
+
+        CompressionAlgorithm::None
+
     }
 }
 
-#[cfg(all(
-    feature = "heuristics",
-    not(any(feature = "heubias-performance", feature = "heubias-ratio"))
-))]
-#[inline]
-#[allow(unused_variables)]
-pub const fn get_compressor(size: usize) -> CompressionAlgorithm {
-    #[cfg(any(feature = "LZMA", feature = "GZIP", feature = "ZSTD", feature = "LZ4"))]
-    if size < 10485760 {
-        #[cfg(feature = "LZMA")]
-        {
-            return CompressionAlgorithm::Lzma;
-        }
-        #[cfg(feature = "GZIP")]
-        {
-            return CompressionAlgorithm::Gzip;
-        }
-        #[cfg(feature = "ZSTD")]
-        {
-            return CompressionAlgorithm::Zstd;
-        }
-        #[cfg(feature = "LZ4")]
-        {
-            return CompressionAlgorithm::Lz4;
-        }
-    }
-    #[cfg(any(feature = "GZIP", feature = "ZSTD", feature = "LZ4"))]
-    if size > 10485760 && size < 209715200 {
-        #[cfg(feature = "GZIP")]
-        {
-            return CompressionAlgorithm::Gzip;
-        }
-        #[cfg(feature = "ZSTD")]
-        {
-            return CompressionAlgorithm::Zstd;
-        }
-        #[cfg(feature = "LZ4")]
-        {
-            return CompressionAlgorithm::Lz4;
-        }
-    }
-    #[cfg(any(feature = "ZSTD", feature = "GZIP"))]
-    if size >= 209715200 && size < 314572800 {
-        #[cfg(feature = "ZSTD")]
-        {
-            return CompressionAlgorithm::Zstd;
-        }
-        #[cfg(feature = "GZIP")]
-        {
-            return CompressionAlgorithm::Gzip;
-        }
-    }
-    #[cfg(feature = "LZ4")]
-    if size >= 314572800 {
-        // 300 mib
-        #[cfg(feature = "LZ4")]
-        {
-            return CompressionAlgorithm::Lz4;
-        }
-    }
-    CompressionAlgorithm::None
-}
-#[cfg(all(feature = "heuristics", feature = "heubias-ratio"))]
-#[inline]
-#[allow(unused_variables)]
-pub const fn get_compressor(size: usize) -> CompressionAlgorithm {
-    #[cfg(any(feature = "LZMA", feature = "ZSTD", feature = "GZIP", feature = "LZ4"))]
-    if size < 10485760 {
-        #[cfg(feature = "LZMA")]
-        {
-            return CompressionAlgorithm::Lzma;
-        }
-        #[cfg(feature = "ZSTD")]
-        {
-            return CompressionAlgorithm::Zstd;
-        }
-        #[cfg(feature = "GZIP")]
-        {
-            return CompressionAlgorithm::Gzip;
-        }
-        #[cfg(feature = "LZ4")]
-        {
-            return CompressionAlgorithm::Lz4;
-        }
-    }
-    #[cfg(any(feature = "ZSTD", feature = "GZIP", feature = "LZMA"))]
-    if size >= 10485760 && size < 209715200 {
-        #[cfg(feature = "ZSTD")]
-        {
-            return CompressionAlgorithm::Zstd;
-        }
-        #[cfg(feature = "LZMA")]
-        {
-            return CompressionAlgorithm::Lzma;
-        }
-        #[cfg(feature = "GZIP")]
-        {
-            return CompressionAlgorithm::Gzip;
-        }
-    }
-    #[cfg(any(feature = "ZSTD", feature = "LZMA"))]
-    if size >= 209715200 {
-        #[cfg(feature = "ZSTD")]
-        {
-            return CompressionAlgorithm::Zstd;
-        }
-        #[cfg(feature = "LZMA")]
-        {
-            return CompressionAlgorithm::Lzma;
-        }
-    }
-
-    CompressionAlgorithm::None
-}
--- src/lib.rs
+++ src/lib.rs
@@ -16,24 +16,8 @@ pub mod enums;
 // Compression level constants per heuristic
 // ===========================================
 
-#[cfg(all(feature = "heuristics", feature = "heubias-performance"))]
-pub mod compression_levels {
-    pub const LZMA_LEVEL: usize = 1;
-    pub const GZIP_LEVEL: usize = 1;
-    pub const ZSTD_LEVEL: usize = 1;
-}
 
-#[cfg(all(feature = "heuristics", feature = "heubias-ratio"))]
-pub mod compression_levels {
-    pub const LZMA_LEVEL: usize = 9;
-    pub const GZIP_LEVEL: usize = 9;
-    pub const ZSTD_LEVEL: usize = 19;
-}
-
-#[cfg(all(
-    feature = "heuristics",
-    not(any(feature = "heubias-ratio", feature = "heubias-performance"))
-))]
+#[cfg(feature="heuristics")]
 pub mod compression_levels {
     pub const LZMA_LEVEL: usize = 4;
     pub const GZIP_LEVEL: usize = 5;
